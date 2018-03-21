120e81d99d1c9f55bd0511995bce3030565b64ae
jbjw <github@jbjw.com>
2018-03-21 16:51:43 -0700

    auto-save

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,16 @@
-const webpack = require('webpack')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const OpenBrowserPlugin = require('open-browser-webpack-plugin')
-
-const path = require('path')
-
-const DIST = path.resolve(__dirname, 'dist')
-const SRC = path.resolve(__dirname, 'src')
+const path = require( "path" )
 
 module.exports = {
-	entry: path.resolve(SRC, 'main.jsx'),
+	mode: "development",
+	entry: "./source/index.js",
 	output: {
-		path: DIST,
-		filename: 'bundle.js',
+		filename: "bundle.js",
+		path: path.resolve( __dirname, "dist" ),
 	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				include: [SRC],
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				options: {
-					presets: ['es2015', 'react'],
-				},
-			},
-			{
-				test: /\.jsx?$/,
-				enforce: 'pre', // what
-				include: [SRC],
-				exclude: /node_modules/,
-				loader: 'eslint-loader',
-				options: {
-					configFile: './.eslintrc',
-				},
-			},
-			{
-				test: /\.css?$/,
-				include: [SRC],
-				exclude: /node_modules/,
-				use: [
-					'style-loader',
-					'css-loader',
-				]
-			},
-		],
-	},
-	plugins: [
-		new CopyWebpackPlugin([
-			{
-				from: 'src/index.html',
-				to: 'index.html'
-			},
-		]),
-
-		new CopyWebpackPlugin([
-			{
-				from: 'src/index.css',
-				to: 'index.css'
-			},
-		]),
-
-		new OpenBrowserPlugin({
-			url: 'http://localhost:3000/'
-		}),
-	],
-	// devServer: {
-	//   historyApiFallback: true,
-	//   host: '0.0.0.0',
-	//   port: 3000,
-	//   stats: 'errors-only',
-	//   noInfo: true,
-	//   watchOptions: {
-	//     aggregateTimeout: 300,
-	//     poll: 1000,
-	//     ignored: /node_modules/
-	//   }
-	// },
-	devtool: 'source-map',
 	devServer: {
-		inline: true,
-		// hot: true,
-		contentBase: 'dist/',
-		port: 80,
+		contentBase: path.join( __dirname, "dist" ),
+		compress: true,
+		port: 9000,
+		https: true,
 	},
 }
